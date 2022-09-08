97567790cac61ead662499bc2d74004979dfdd35
Mason Guaman <masonstalin@gmail.com>
2022-09-08 13:33:41 -0400

    ss

--- Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,12 +10,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Calling")
+print("By Sleepy Hallow")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("I know you only be calling when it's lit")
+print("Ten toes down with the mob, and we ain't slip")
 
 
 /*:
